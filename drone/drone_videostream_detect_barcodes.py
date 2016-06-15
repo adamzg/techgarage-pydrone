03f5baefe2db3d5f0a7c9182abc548a412c24dd1
vsafin <vsafin@kdsconsulting.com>
2016-06-15 02:08:07 -0400

    Encapsulated barcode detection in separate class

--- drone/drone_videostream_detect_barcodes.py
+++ drone/drone_videostream_detect_barcodes.py
@@ -1,42 +1,29 @@
 import cv2
 import numpy as np
-import zbar
-import PIL.Image as Image
 from bebop import Bebop
+from bardecoder import Decoder
+from bardecoder import Barcode
 
 cnt = 0
 
-scanner = zbar.ImageScanner()
-scanner.parse_config('enable')
+decoder = Decoder()
+
 
 def videoCallback( frame, drone, debug=False ):
-    global cnt
-    cnt = cnt + 1
-    print cnt
+    global decoder
     if isinstance(frame, tuple):
         print "h.264 frame - (frame# = %s, iframe = %s, size = %s)" % (frame[0], frame[1], len(frame[2]))
 
     else:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        pil = Image.fromarray(gray)
-        width, height = pil.size
-        raw = pil.tobytes()
-
-        # wrap image data
-        image = zbar.Image(width, height, 'Y800', raw)
-
-        # scan the image for barcodes
-        scanner.scan(image)
-
-        # extract results
-        for symbol in image:
-            # do something useful with results
-            print 'decoded', symbol.type, 'symbol', symbol.location, '"%s"' % symbol.data
-            print symbol
-            cv2.line(frame, symbol.location[0], symbol.location[1], color=(0, 0, 255), thickness=2)
-            cv2.line(frame, symbol.location[1], symbol.location[2], color=(0, 0, 255), thickness=2)
-            cv2.line(frame, symbol.location[2], symbol.location[3], color=(0, 0, 255), thickness=2)
-            cv2.line(frame, symbol.location[0], symbol.location[3], color=(0, 0, 255), thickness=2)
+        barcodes = decoder.decode(frame)
+        if len(barcodes) > 0:
+            for barcode in barcodes:
+                # do something useful with results
+                print 'decoded', barcode.type, 'symbol', barcode.location, '"%s"' % barcode.value
+                cv2.line(frame, barcode.location[0], barcode.location[1], color=(0, 0, 255), thickness=2)
+                cv2.line(frame, barcode.location[1], barcode.location[2], color=(0, 0, 255), thickness=2)
+                cv2.line(frame, barcode.location[2], barcode.location[3], color=(0, 0, 255), thickness=2)
+                cv2.line(frame, barcode.location[0], barcode.location[3], color=(0, 0, 255), thickness=2)
 
         cv2.imshow("Drone feed", frame)
         cv2.waitKey(10)
