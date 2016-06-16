b95b386777f5cb0321b08eb053173a8c11698fcc
vsafin <vsafin@kdsconsulting.com>
2016-06-16 10:47:42 -0400

    package delivery

--- drone/drone_videostream_detect_barcodes.py
+++ drone/drone_videostream_detect_barcodes.py
@@ -1,3 +1,5 @@
+from email.iterators import body_line_iterator
+
 import cv2
 import numpy as np
 from bebop import Bebop
@@ -20,10 +22,35 @@ def videoCallback( frame, drone, debug=False ):
             for barcode in barcodes:
                 # do something useful with results
                 print 'decoded', barcode.type, 'symbol', barcode.location, '"%s"' % barcode.value
-                cv2.line(frame, barcode.location[0], barcode.location[1], color=(0, 0, 255), thickness=2)
-                cv2.line(frame, barcode.location[1], barcode.location[2], color=(0, 0, 255), thickness=2)
-                cv2.line(frame, barcode.location[2], barcode.location[3], color=(0, 0, 255), thickness=2)
-                cv2.line(frame, barcode.location[0], barcode.location[3], color=(0, 0, 255), thickness=2)
+                min_x = min(barcode.location[0][0], barcode.location[1][0], barcode.location[2][0], barcode.location[3][0])
+                max_x = max(barcode.location[0][0], barcode.location[1][0], barcode.location[2][0], barcode.location[3][0])
+
+                min_y = min(barcode.location[0][1], barcode.location[1][1], barcode.location[2][1], barcode.location[3][1])
+                max_y = max(barcode.location[0][1], barcode.location[1][1], barcode.location[2][1], barcode.location[3][1])
+
+                center_x = int( (min_x + max_x) * 0.5)
+                center_y = int( (min_y + max_y) * 0.5)
+
+                height, width, _ = frame.shape
+
+                color = (0, 0 , 255)
+                text = "Centered"
+                if abs(width * 0.5 - center_x) < 50:
+                    color = (0, 255, 0)
+                elif center_x - width * 0.5 > 50:
+                    color = (0, 0, 255)
+                    text = "Go Right"
+                elif center_x - width * 0.5 < 50:
+                    color = (255, 0, 0)
+                    text = "Go Left"
+
+                cv2.line(frame, barcode.location[0], barcode.location[1], color=color, thickness=2)
+                cv2.line(frame, barcode.location[1], barcode.location[2], color=color, thickness=2)
+                cv2.line(frame, barcode.location[2], barcode.location[3], color=color, thickness=2)
+                cv2.line(frame, barcode.location[0], barcode.location[3], color=color, thickness=2)
+                cv2.circle(frame, (center_x, center_y), 3, color=color, thickness=2)
+                cv2.putText(frame, org=(width - 100, height - 100), text=text, color=color, fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,)
+                break
 
         cv2.imshow("Drone feed", frame)
         cv2.waitKey(10)
--- drone/joystick_teleop_template.py
+++ drone/joystick_teleop_template.py
@@ -1,18 +1,5 @@
 import pygame
-import cv2
-from commands import *
-from bebop import Bebop
 
-def videoCallback( frame, drone, debug=False ):
-    cv2.imshow("image", frame)
-    cv2.waitKey(10)
-
-
-print "Connecting to drone.."
-drone = Bebop( metalog=None, onlyIFrames=False, jpegStream=True )
-drone.videoCbk = videoCallback
-drone.videoEnable()
-print "Connected."
 
 pygame.init()
 size = [100, 100]
@@ -34,79 +21,50 @@ else:
     print "Initialized %s" % (joystick.get_name())
     print "Number of buttons %d. Number of axis %d, Number of hats %d" % (joystick.get_numbuttons(), joystick.get_numaxes(), joystick.get_numhats())
 
-
 # -------- Main Program Loop -----------
 
-MAX_SPEED = 30
-
 while not done:
-
     # EVENT PROCESSING STEP
-    for event in pygame.event.get():  # User did something
+    for event in pygame.qevent.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
 
-    executing_command = False
-
     if joystick.get_button(0) == 1:
-        executing_command = True
-        print "Landing..."
-        if drone.flyingState is None or drone.flyingState == 1: # if taking off then do emegency landing
-            drone.emergency()
-        drone.land()
+        print "Button 0 pressed"
 
     if joystick.get_button(1) == 1:
-        executing_command = True
-        drone.update(cmd=movePCMDCmd(True, 0, 0, MAX_SPEED, 0))
-        print("Drone spinning clockwise.")
+        print "Button 1 pressed"
 
     if joystick.get_button(2) == 1:
-        executing_command = True
-        drone.update(cmd=movePCMDCmd(True, 0, 0, -1 * MAX_SPEED, 0))
-        print("Drone spinning counterclockwise.")
+        print "Button 2 pressed"
 
     if joystick.get_button(3) == 1:
-        executing_command = True
-        print "Taking off.."
-        drone.takeoff()
+        print "Button 3 pressed"
 
     if joystick.get_button(4) == 1:
-        executing_command = True
-        print "Descending..."
-        drone.update(cmd=movePCMDCmd(True, 0, 0, 0, -1 * MAX_SPEED))
+        print "Button 4 pressed"
 
     if joystick.get_button(5) == 1:
-        executing_command = True
-        print "Asccending..."
-        drone.update(cmd=movePCMDCmd(True, 0, 0, 0, MAX_SPEED))
+        print "Button 5 pressed"
 
     if joystick.get_button(6) == 1:
-        executing_command = True
         print "Button 6 pressed"
 
     if joystick.get_button(7) == 1:
-        executing_command = True
         print "Button 7 pressed"
 
     if abs(joystick.get_axis(0)) > 0.05 or abs(joystick.get_axis(1)) > 0.05:
-        executing_command = True
         print "Left stick %f, %f" % (joystick.get_axis(0), joystick.get_axis(1))
 
     if abs(joystick.get_axis(3)) > 0.05 or abs(joystick.get_axis(4)) > 0.05:
-        executing_command = True
         print "Right stick %f, %f" % (joystick.get_axis(3), joystick.get_axis(4))
 
     (hat_x, hat_y) = joystick.get_hat(0)
     if (hat_x != 0 or hat_y !=0):
-        executing_command = True
         print "Hat %d, %d" % (hat_x, hat_y)
 
     # Limit to 20 frames per second
     clock.tick(20)
-
-    if not executing_command:
-        drone.update(cmd=movePCMDCmd(False, 0, 0, 0, 0))
-        drone.update(cmd=None)
 
 # Close the window and quit.
 pygame.quit()
